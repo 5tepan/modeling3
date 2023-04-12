1dfed931d8e724ff29f0642a527c3b6bfe79e920
5tepan <f.stepa.n@yandex.ru>
2023-04-12 21:18:35 +0300

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const port = 3001;
+const routes = require("./routes");
+require('dotenv').config();
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_DB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(routes);
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+  });
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from "react";
+import "./App.css";
+
+import {createLink, deleteLink, getLinks} from "./api";
+
+const API_URL = process.env.REACT_APP_URL;
+const INVALID_CLASS = 'invalid';
+
+const isValidUrl = (url) => {
+    const objRE = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    return objRE.test(url);
+}
+
+function App() {
+    const [links, setLinks] = useState([]);
+
+    useEffect(() => {
+        updateLinkList()
+    }, []);
+
+    const updateLinkList = () =>{
+        getLinks().then((data) => {
+            setLinks(Object.entries(data));
+        });
+    }
+
+    const createNewLink = () => {
+        const input = document.querySelector('#newLink');
+        let url = input.value;
+        if (!isValidUrl(url)) {
+            url = "https://" + url;
+            if (!isValidUrl(url)) {
+                console.log(`"${url}" is invalid`);
+                input.classList.add(INVALID_CLASS);
+                return;
+            }
+        }
+        input.classList.remove(INVALID_CLASS);
+
+        input.value = "";
+        createLink(url).then(() => updateLinkList());
+    }
+
+    const deleteUrl = (id) => {
+        deleteLink(id).then(() => updateLinkList())
+    }
+
+    return (
+        <div className="container">
+            <h1 className='title'>Сократите ссылку!</h1>
+            <div>
+                <input className='inputLink' id="newLink" type="text" placeholder="Введите ссылку"/>
+                {/*<button className='buttonSave' onClick={() => createNewLink()} >Сохранить</button>*/}
+                <a href="#" className="buttonSave" onClick={() => createNewLink()} >Сохранить</a>
+            </div>
+            <div>
+                <ul>
+                    {links.map(([id, value]) => {
+                        const link = `${API_URL}/${value.shortLink}`;
+                        return (
+                            <li>
+                                <a className={link} id={id} href={link}>{link}</a>
+                                <a href="#" className="buttonSave" onClick={() => deleteUrl(id)} >Удалить</a>
+                                {/*<button onClick={() => deleteUrl(id)}>Delete</button>*/}
+                            </li>
+                        )}
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
